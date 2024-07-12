66cd418c44c9f40614329b272bab228414518b76
Evilazaro <evilazaro@gmail.com>
2024-07-12 13:04:25 -0500

    updating dependencies script

--- src/bash/updateDependencies.sh
+++ src/bash/updateDependencies.sh
@@ -21,22 +21,8 @@ function updateDependencies
 
 # Function to check the package manager and update packages
 update_packages() {
-  if command -v apt-get &> /dev/null; then
-    echo "Updating packages using apt-get..."
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
-    sudo apt-get dist-upgrade -y
-    sudo apt-get autoremove -y
-    sudo apt-get autoclean -y
-  elif command -v yum &> /dev/null; then
-    echo "Updating packages using yum..."
-    sudo yum check-update -y
-    sudo yum update -y
-    sudo yum clean all
-  else
-    echo "Unsupported package manager. Please update manually."
-    exit 1
-  fi
+  sudo apt-get update && \
+  sudo apt-get upgrade -y 
 }
 
 # Function to install or update .NET SDK 8.0 on Ubuntu
