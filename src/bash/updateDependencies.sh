5ae811e393f423a19d3623244ab7a374fa79a87f
Evilázaro Alves <evilazaro@gmail.com>
2024-07-17 12:13:14 -0500

    settings file

--- src/bash/updateDependencies.sh
+++ src/bash/updateDependencies.sh
@@ -47,29 +47,26 @@ install_azure_cli() {
 function InstallVSCodeExtensions {
     echo "Installing VSCode extensions..."
 
-    extensions=(
-        "ms-vscode-remote.remote-wsl"
-        "ms-vscode.PowerShell"
-        "ms-vscode.vscode-node-azure-pack"
-        "GitHub.copilot"
-        "GitHub.vscode-pull-request-github"
-        "GitHub.copilot-chat"
-        "GitHub.remotehub"
-        "GitHub.vscode-github-actions"
-        "eamodio.gitlens-insiders"
-        "ms-vscode.azure-repos"
-        "ms-azure-devops.azure-pipelines"
-        "ms-azuretools.vscode-docker"
-        "ms-kubernetes-tools.vscode-kubernetes-tools"
-        "ms-kubernetes-tools.vscode-aks-tools"
-        "ms-azuretools.vscode-azurecontainerapps"
-        "ms-azuretools.vscode-azurefunctions"
-        "ms-azuretools.vscode-apimanagement"
-    )
 
-    for extension in "${extensions[@]}"; do
-        code --install-extension $extension --force
-    done
+    code --install-extension "ms-vscode-remote.remote-wsl" --force
+    code --install-extension "ms-vscode.PowerShell" --force
+    code --install-extension "ms-vscode.vscode-node-azure-pack" --force
+    code --install-extension "GitHub.copilot" --force
+    code --install-extension "GitHub.vscode-pull-request-github" --force
+    code --install-extension "GitHub.copilot-chat" --force
+    code --install-extension "GitHub.remotehub" --force
+    code --install-extension "GitHub.vscode-github-actions" --force
+    code --install-extension "eamodio.gitlens-insiders" --force
+    code --install-extension "ms-vscode.azure-repos" --force
+    code --install-extension "ms-azure-devops.azure-pipelines" --force
+    code --install-extension "ms-azuretools.vscode-docker" --force
+    code --install-extension "ms-kubernetes-tools.vscode-kubernetes-tools" --force
+    code --install-extension "ms-kubernetes-tools.vscode-aks-tools" --force
+    code --install-extension "ms-azuretools.vscode-azurecontainerapps" --force
+    code --install-extension "ms-azuretools.vscode-azurefunctions" --force
+    code --install-extension "ms-azuretools.vscode-apimanagement" --force
+
+
 
     echo "VSCode extensions have been installed successfully."
 }
