c38e2e719595fc36ebb57d7717273af818306a77
Evilázaro Alves <evilazaro@gmail.com>
2024-07-23 13:26:50 -0500

    reducing the installation steps to 2

--- src/powershell/configureDevMachine.ps1
+++ src/powershell/configureDevMachine.ps1
@@ -1,6 +1,6 @@
 param(
-    [Parameter(Mandatory=$true, HelpMessage="Please provide the installation step number between 1 and 3.")]
-    [int]$step = 2
+    [Parameter(Mandatory=$true, HelpMessage="Please provide the installation step number between 1 and 2.")]
+    [int]$step = 1
 )
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
@@ -193,12 +193,10 @@ switch ($step) {
     }
     2 {
         importWingetPackages
-        restartComputer
-    }
-    3 {
         InstallVSCodeExtensions
         UpdateDotNetWorkloads   
-        updateWingetPackages     
+        updateWingetPackages 
+        restartComputer
     }
     default {
         Write-Host "Invalid step number. Please provide a valid step number." -Level "ERROR"
