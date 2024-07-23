1f7e226ef1eb27fe86f3f20ad65ffeda5df76f2c
Evilázaro Alves <evilazaro@gmail.com>
2024-07-23 15:01:26 -0500

    step 3 added again

--- src/powershell/configureDevMachine.ps1
+++ src/powershell/configureDevMachine.ps1
@@ -1,5 +1,5 @@
 param(
-    [Parameter(Mandatory=$true, HelpMessage="Please provide the installation step number between 1 and 2.")]
+    [Parameter(Mandatory=$true, HelpMessage="Please provide the installation step number between 1 and 3.")]
     [int]$step = 1
 )
 
@@ -193,10 +193,12 @@ switch ($step) {
     }
     2 {
         importWingetPackages
+        restartComputer
+    }
+    3 {
         InstallVSCodeExtensions
         UpdateDotNetWorkloads   
         updateWingetPackages 
-        restartComputer
     }
     default {
         Write-Host "Invalid step number. Please provide a valid step number." -Level "ERROR"
